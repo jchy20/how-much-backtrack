94eb1418b015500ec14ee6b46373f093188134e3
jchy20 <james1587435959@gmail.com>
2025-03-02 16:05:32 -0500

    train_script

--- scripts/slurm_train.sh
+++ scripts/slurm_train.sh
@@ -5,8 +5,8 @@
 #SBATCH --gres=gpu:a6000:4
 #SBATCH --mem=500G
 #SBATCH --cpus-per-task=10
-#SBATCH --job-name=list_functions-qwen3b
-#SBATCH --output=slurm_logs/list_functions-qwen3b.out
+#SBATCH --job-name=arc_1d-qwen3b
+#SBATCH --output=slurm_logs/arc_1d-qwen3b.out
 
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate zero
@@ -15,9 +15,9 @@ zsh -l -c '
 # Set environment variables
 export N_GPUS=4
 export BASE_MODEL=/usr/xtmp/hc387/models/Qwen2.5-3B-Instruct
-export DATA_DIR=/home/users/hc387/data/list_functions
+export DATA_DIR=/home/users/hc387/data/arc_1d
 export ROLLOUT_TP_SIZE=2
-export EXPERIMENT_NAME=list_functions
+export EXPERIMENT_NAME=arc_1d
 export VLLM_ATTENTION_BACKEND=XFORMERS
 
 # Run the training script
